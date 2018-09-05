72ed09f8b1943bfa9749fcc2217fd3e8555713e3
Nitzan Frock <nitzanfrock@gmail.com>
2018-09-05 16:15:50 -0400

    setup corner and edges cases for neighboring boards.

--- pset4-8_puzzle-priority-queues/src/Board.java
+++ pset4-8_puzzle-priority-queues/src/Board.java
@@ -1,9 +1,14 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import edu.princeton.cs.algs4.MinPQ;
 
 public class Board {
 	private int dimension;
 	private int[][] blocks;
 	private int[][] goal;
+	private int hamming;
+	private int manhattan;
 	
 	// construct a board from an n-by-n array of blocks (where blocks[i][j] = block in row i, column j)
 	public Board(int[][] blocks) {			
@@ -19,6 +24,9 @@ public class Board {
 				goal[i][j] = count++;
 			}
 		}
+		
+		calcHamming();
+		calcManhattan();
 	}
 	
 	// board dimension n
@@ -28,7 +36,11 @@ public class Board {
 	
 	// number of blocks out of place
 	public int hamming() {
-		int hamming = 0;
+		return hamming;
+	}
+	
+	private void calcHamming() {
+		hamming = 0;
 		for (int i = 0; i < dimension; i++) {
 			for (int j = 0; j < dimension; j++) {
 				if (blocks[i][j] != goal[i][j]) {
@@ -36,12 +48,15 @@ public class Board {
 				}
 			}
 		}
-		return hamming;
 	}
 	
 	// sum of Manhattan distances between blocks and goal
 	public int manhattan() {
-		int manhattan = 0;
+		return manhattan;
+	}
+	
+	private void calcManhattan() {
+		manhattan = 0;
 		for (int i = 0; i < dimension; i++) {
 			for (int j = 0; j < dimension; j++) {
 				if (blocks[i][j] != goal[i][j]) {
@@ -54,7 +69,6 @@ public class Board {
 				}
 			}
 		}
-		return manhattan;
 	}
 	
 	private int xyTo1D(int y, int x) {
@@ -89,17 +103,55 @@ public class Board {
 		temp[0][1] = swap;
 		
 		return new Board(temp);
-		
 	}
 	
 	// does this board equal y?
 	public boolean equals(Object y) {
-		
+		return false;
 	}
 	
 	// all neighboring boards
 	public Iterable<Board> neighbors(){
+		return new Neighbors(blocks);
+	}
+	
+	private class Neighbors implements Iterable<Board>{
+		private ArrayList<Board> neighbors;
 		
+		private Neighbors(int[][] blocks) {
+			for (int i = 0; i < dimension; i++) {
+				for (int j = 0; j < dimension; j++) {
+					if (blocks[i][j] == 0) {
+						// corner cases:
+						// top right: [0][dimension-1]
+						// top left: [0][0]
+						// bottom left: [dimension-1][0]
+						// bottom right: [dimension-1][dimension-1]
+						
+						// edge cases:
+						// top: [0][0 < x < dimension -1]
+						// left: [0 < x < dimension-1][0]
+						// bottom: [dimension-1][0 < x < dimension-1]
+						// right: [0 < x < dimension-1][dimension-1]
+					}
+				}
+			}
+		}
+		
+		public Iterator<Board> iterator() {
+			return new NeighborsIterator();
+		}
+		
+	}
+	
+	private class NeighborsIterator implements Iterator<Board>{
+		public boolean hasNext() {
+			return false;
+		}
+
+		public Board next() {
+			return null;
+		}
 	}
 	
 	// string representation of this board (in the output format specified below)
