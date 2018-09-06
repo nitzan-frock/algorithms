9ab277d73cdd65dd7650548d998f1983ad282a27
Frock <nitzanf@novatec.com>
2018-09-06 16:05:22 -0400

    WIP neighbors

--- pset4-8_puzzle-priority-queues/src/Board.java
+++ pset4-8_puzzle-priority-queues/src/Board.java
@@ -15,13 +15,13 @@ public class Board {
 		dimension = blocks.length;
 		this.blocks = blocks;
 		
-		int count = 1;
+		int position = 1;
 		for (int i = 0; i < dimension; i++) {
 			for (int j = 0; j < dimension; j++) {
-				if (count == dimension*dimension) {
+				if (position == dimension*dimension) {
 					goal[i][j] = 0;
 				}
-				goal[i][j] = count++;
+				goal[i][j] = position++;
 			}
 		}
 		
@@ -57,10 +57,10 @@ public class Board {
 	
 	private void calcManhattan() {
 		manhattan = 0;
+		int n2 = dimension*dimension;
 		for (int i = 0; i < dimension; i++) {
 			for (int j = 0; j < dimension; j++) {
 				if (blocks[i][j] != goal[i][j]) {
-					int n2 = dimension*dimension;
 					int goal_i = n2 - (n2 - blocks[i][j]) - 1;
 					int current_i = xyTo1D(i, j);
 					int rowsOOP = Math.abs(goal_i - current_i) % dimension; // rows out of place
@@ -122,35 +122,80 @@ public class Board {
 			for (int i = 0; i < dimension; i++) {
 				for (int j = 0; j < dimension; j++) {
 					if (blocks[i][j] == 0) {
-						// corner cases:
-						// top right: [0][dimension-1]
-						// top left: [0][0]
-						// bottom left: [dimension-1][0]
-						// bottom right: [dimension-1][dimension-1]
-						
-						// edge cases:
-						// top: [0][0 < x < dimension -1]
-						// left: [0 < x < dimension-1][0]
-						// bottom: [dimension-1][0 < x < dimension-1]
-						// right: [0 < x < dimension-1][dimension-1]
+						initializeNeighbors(blocks, i, j);
 					}
 				}
 			}
+		}
+		
+		private void initializeNeighbors(int[][] blocks, int row, int col) {
+			for (int i = -1; i <= 1; i++) {
+				for (int j = -1; j <= 1; j++) {
+					if (!(i == 0 && j == 0)) {
+						int[][] copy = copyBlocks(blocks);
+						
+					}
+				}
+			}
+			if (isTopEdge(row)){
+				if(isLeftEdge(col)) {
+					// top left: [0][0]
+					
+				}else if(isRightEdge(col)) {
+					// top right: [0][dimension-1]
+				}
+				
+			}else if (isBottomEdge(row)) {
+				if(isLeftEdge(col)) {
+					// bottom left: [dimension-1][0]
+				}else if(isRightEdge(col)) {
+					// bottom right: [dimension-1][dimension-1]
+				}
+			} else if (isRightEdge(col)) {
+				
+			} else if (isLeftEdge(col)) {
+				
+			}
+		}
+		
+		private int[][] copyBlocks(int[][] blocks){
+			int[][] copy = new int[dimension][dimension];
+			for (int i = 0; i < dimension; i++) {
+				for (int j = 0; j < dimension; j++) {
+					copy[i][j] = blocks[i][j];
+				}
+			}
+			return copy;
+		}
+		
+		private boolean isTopEdge(int row) {
+			return row == 0;
+		}
+		
+		private boolean isBottomEdge(int row) {
+			return row == dimension-1;
+		}
+		
+		private boolean isLeftEdge(int col) {
+			return col == 0;
+		}
+		
+		private boolean isRightEdge(int col) {
+			return col == dimension-1;
 		}
 		
 		public Iterator<Board> iterator() {
 			return new NeighborsIterator();
 		}
 		
-	}
-	
-	private class NeighborsIterator implements Iterator<Board>{
-		public boolean hasNext() {
-			return false;
-		}
+		private class NeighborsIterator implements Iterator<Board>{
+			public boolean hasNext() {
+				return false;
+			}
 
-		public Board next() {
-			return null;
+			public Board next() {
+				return null;
+			}
 		}
 	}
 	
